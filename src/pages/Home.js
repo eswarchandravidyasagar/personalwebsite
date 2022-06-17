eb47bf934f114cabaa911f5638d4b368e1de8ba5
Eswarchandra <eswarchandra111@gmail.com>
2022-06-17 11:21:01 -0400

    updates in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,14 +4,18 @@ import LinkeinIcon from "@mui/icons-material/LinkedIn";
 import GithubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email"
 
+import {Card, CardContent, Typography, Grid} from "@mui/material";
+
+
 
 function Home() {
   return (
     <div className='home'>
+      
     <div className='about'>
       <h2>Hi,My name is Eswar</h2>
       <div className='prompt'>
-        <p>I am a passionate programmer with problem solving skills</p>
+        <p>I am a Passionate Web Devloper With Creative Problem Solving Skills</p>
         <a href='https://www.linkedin.com/in/eswarattuluri/' ><LinkeinIcon /></a>
         <a href='https://github.com/eswarchandravidyasagar'><GithubIcon /></a>
         <a href={"/contact"} ><EmailIcon /></a>
@@ -20,18 +24,10 @@ function Home() {
 
     <div className='skills'>
       <h1>Skills</h1>
-      <ol className='list'>
-        <li className='item'>
-          <h2>Front End </h2>
-            <span>Html,CSS,Javascript,React,Bootstrap</span>
-          
-        </li>
-        <li className='item'>
-          <h2>Backend skills</h2>
-          <span>Node.js, MongoDB ,Expressjs</span>
-        </li>
-        <li className='item'></li>
-      </ol>
+      <h2>Front end skills</h2>
+      <p>HTML,CSS,Javascript,React,bootstrap,MaterialUI</p>
+      <h2>Front end skills</h2>
+      <p>Node.js,MongoDB,Express.js</p>
 
 
     </div>
