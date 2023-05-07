5b0aadc60ec7fa473ea6f49fdafbdad9990e33e1
Eswarchandra <eswarchandra111@gmail.com>
2023-05-07 15:34:01 -0400

    homepageFix

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+          <Route path="/personalwebsite" element={<Home />} />
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/experience" element={<Experience />} />
